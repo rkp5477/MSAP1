ab5c538c9309d846c84ad1cb57542904ff4388d6
rkp5477 <rkp5477@autuni.ac.nz>
2019-07-07 18:08:04 +1200

    First Commit

---
+++ schoolSIMS/Model/schoolSIMScontext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace schoolSIMS.Model
+{
+    public partial class schoolSIMScontext : DbContext
+    {
+        public schoolSIMScontext()
+        {
+        }
+
+        public schoolSIMScontext(DbContextOptions<schoolSIMScontext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:rkp5477schoolsims.database.windows.net,1433;Initial Catalog=schoolSIMS;Persist Security Info=False;User ID=rkp5477;Password=schoolSIMS1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.AddressName).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+        }
+    }
+}
